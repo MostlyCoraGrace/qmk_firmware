2afbb0147c6396ea87da26e8a93d4706e2fea0af
Cora <hamish.grace@hotmail.com>
2023-08-01 21:09:27 +1000

    updated keymap

--- keyboards/sofle/keymaps/MostlyCoraGrace/keymap.c
+++ keyboards/sofle/keymaps/MostlyCoraGrace/keymap.c
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_QR] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+   KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   _______,  KC_F20,  KC_F22,  KC_F23,  KC_F24, XXXXXXX,                         KC_PAUS, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  KC_F12,
-  _______,  KC_F17,  KC_F18,  KC_F19,  KC_F20, XXXXXXX,                         KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_INS,
+   KC_DEL,  KC_F17,  KC_F18,  KC_F19,  KC_F20, XXXXXXX,                         KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_INS,
   KC_CAPS,  KC_F13,  KC_F14,  KC_F15,  KC_F16, XXXXXXX, _______,       _______, KC_SCRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS,
                     _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|RGB OFF|    |       |------+------+------+------+------+------|
  * |   Z  | CTRL |   G  |   M  |   P  |   T  |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |   X  |   C  |   V  | SPACE| /  ALT  /       \      \  |      |      |      |      |
+ *            |   X  |   C  |   V  |  ALT | / SPACE /       \      \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,    KC_6,    KC_Q,    KC_W,    KC_E,    KC_R,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   MO(_GL), KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      KC_Z, KC_LCTL,    KC_G,    KC_M,    KC_P,    KC_T, _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                       KC_X,    KC_C,    KC_V,  KC_SPC, KC_LALT,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+                       KC_X,    KC_C,    KC_V, KC_LALT,  KC_SPC,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
  /* GAME LOWER
